236a984f0a51fcf26fed7e742b2e355ebffb3b47
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-12-15 15:13:55 -0500

    Update index.php
    Ajouter les conditions pour gérer les erreurs.
    (quand il y a une erreur, rediriger vers la page d'erreur '/home/error'.

--- index.php
+++ index.php
@@ -6,37 +6,51 @@
 
     // récupérer le chemin
     $url = isset($_GET['url']) ? explode('/', ltrim($_GET['url'], '/')) : '/';
-
+    
     if($url == '/'){
         // page d'accueil (home-index) (par défaut)
         require_once('controllers/ControllerHome.php');
         $controller = new ControllerHome();
         echo $controller->index();
-    } else{
+    }
+    else{
         // chemin du controller
         $requestURL = ucfirst($url[0]);
         $controllerPath = __DIR__.'/controllers/Controller'.$requestURL.'.php';
-
+        
         if(file_exists($controllerPath)){
             // récupérer le controller
             require_once($controllerPath);
             $controllerName = 'Controller'.$requestURL;
             $controller = new $controllerName();
-
+            
             if(isset($url[1])){
                 // récupérer la méthode
                 $method = $url[1];
+                
                 if(isset($url[2])){
+                    // afficher la page avec sa valeur
                     $value = $url[2];
                     echo $controller->$method($value);
-                } else{
-                    echo $controller->$method();
+                }
+                else{
+                    if(method_exists($controller, $method)){
+                        // afficher la page
+                        echo $controller->$method();
+                    }
+                    else{
+                        // page d'erreur (home-error)
+                        require_once('controllers/ControllerHome.php');
+                        $controller = new ControllerHome();
+                        echo $controller->error();
+                    }
                 }
             } else{
-                // page controller-index
+                // page controller-index (par défaut)
                 echo $controller->index();
             }
-        } else{
+        }
+        else{
             // page d'erreur (home-error)
             require_once('controllers/ControllerHome.php');
             $controller = new ControllerHome();
